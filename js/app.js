bd7ba649668068ca357e2da06ac73588a0b20b21
Anubhab Mukherjee <anubhabmukherjee5@gmail.com>
2025-01-03 21:06:03 +0530

    JS

---
+++ js/app.js
@@ -0,0 +1,8 @@
+console.log("learning JAVASCRIPT");
+console.log(1 + 4);
+// use keyword debugger
+// add the breakpoint directly in browser sources tab
+
+/**
+ * variables, operators, data types, objects, functions
+ */
